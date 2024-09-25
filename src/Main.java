1f3746bd4e876d021d6d76c9b37e2b630665725b
srvl <servolo.silva@alunos.ufersa.edu.br>
2024-09-25 13:34:56 -0300

    Exclusão dados do main

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,6 @@ import controllers.UsuarioControle;
 
 public class Main {
     public static void main(String[] args) {
-        String nome = "asdsadad";
-        String documento = "testeFINAL4";
-        String telefone = "91231324";
-        String email = "ated@gmail.com";
-        String senha = "new22";
-
-        System.out.println("Valores enviados:");
-        System.out.println("Nome: " + nome);
-        System.out.println("Documento: " + documento);
-        System.out.println("Telefone: " + telefone);
-        System.out.println("Email: " + email);
-        System.out.println("Senha: " + senha);
-
-        
-        UsuarioControle.cadastrarUsuario(nome, documento, telefone, email, senha);
+       
     }
 }
