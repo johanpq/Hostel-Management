9c96f05c8861c6fbcf3be88d6c947c4a60f4036e
johanpq <johanp.queiroz@gmail.com>
2024-10-02 19:15:40 -0300

    Atualizado

--- src/run.sh
+++ src/run.sh
@@ -4,7 +4,7 @@
 mkdir -p ../bin
 
 # Compilar os arquivos Java
-javac -d ../bin -cp "lib/*" ../src/config/ConnectionFactory.java ../src/Main.java
+javac -d ../bin -cp "lib/*" ../src/config/ConnectionFactory.java  ../src/models/Usuario.java ../src/models/Funcionario.java ../src/dao/FuncionarioDAO.java ../src/models/Quarto.java ../src/dao/QuartoDAO.java ../src/views/QuartoView.java ../src/views/FuncionarioView.java ../src/models/Hospede.java ../src/dao/HospedeDAO.java ../src/Main.java
 
 # Executar o programa principal
 java -cp "lib/*:../bin" Main
