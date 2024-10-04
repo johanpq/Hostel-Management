5972b52ccf3b9140ba58f8779f00251916fac8bd
johanpq <johanp.queiroz@gmail.com>
2024-10-03 21:22:18 -0300

    feat: implementado lógica para o funcionário admin

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 import config.ConnectionFactory;
+import views.FuncionarioView;
+import views.HospedeView;
+import views.QuartoView;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,16 +30,27 @@ public class Main {
                         boolean ehFuncAdmin = (ehAdmin == 1);
     
                         input.nextLine();  
-                        System.out.println("----------LOGIN----------");
-    
-                        System.out.print("Email: ");
-                        String email = input.nextLine();
-    
-                        System.out.print("Senha: ");
-                        String senha = input.nextLine();
-    
-                        checarAdmin(input, email, senha, ehFuncAdmin);
-    
+
+                        if(ehFuncAdmin) {
+                            System.out.println("----------LOGIN----------");
+        
+                            System.out.print("Email: ");
+                            String email = input.nextLine();
+        
+                            System.out.print("Senha: ");
+                            String senha = input.nextLine();
+        
+                            loginDeuCerto = checarAdmin(input, email, senha, ehFuncAdmin);
+
+                            if(loginDeuCerto) {
+                                mostrarMenu(input);
+                            }
+
+                            System.out.println("-------------------------");
+                        } else {
+                            System.err.println("É funcionário mais sem ser Admin");
+                        }
+
                         break;
     
                     case 2:
@@ -49,52 +63,55 @@ public class Main {
     
             } catch (Exception e) {
                 System.out.println("Ocorreu uma exceção! Tente novamente!");
-            } finally {
-                input.close();
-            }
+            } 
         } while(!loginDeuCerto);
-
+        input.close();
     }
 
     public static void mostrarMenu(Scanner input) {
-        try {
-            System.out.println("---------MENU----------");
-            System.out.println(" 1. Hospede");
-            System.out.println(" 2. Funcionario");
-            System.out.println(" 3. Quarto");
-            System.out.println(" 4. Reserva");
-            System.out.println(" 5. Sair");
-            System.out.println("-----------------------");
-            System.out.print("-> ");
-            int escolha = input.nextInt();
-
-            switch (escolha) {
-                case 1:
-                    System.out.println("Case 1");
-                    break;
-                case 2:
-                    System.out.println("Case 2");
-                    break;
-                case 3:
-                    System.out.println("Case 3");
-                    break;
-                case 4:
-                    System.out.println("Case 4");
-                    break;
-                case 5:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
+        boolean sair = false;
+        do {
+            try {
+                System.out.println("---------MENU----------");
+                System.out.println(" 1. Hospede");
+                System.out.println(" 2. Funcionario");
+                System.out.println(" 3. Quarto");
+                System.out.println(" 4. Reserva");
+                System.out.println(" 5. Sair");
+                System.out.println("-----------------------");
+                System.out.print("-> ");
+                int escolha = input.nextInt();
+    
+                switch (escolha) {
+                    case 1:
+                        HospedeView.gerenciarHospede();
+                        break;
+                    case 2:
+                        FuncionarioView.gerenciarFuncionarios();
+                        break;
+                    case 3:
+                        QuartoView.gerenciarQuartos();
+                        break;
+                    case 4:
+                        System.out.println("Case 4");
+                        break;
+                    case 5:
+                        sair = true;
+                        System.out.println("Saindo...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                }
+            } catch (Exception e) {
+                System.out.println("Valor inválido!");
+            } finally {
+                input.nextLine(); // Limpar o buffer
             }
-        } catch (Exception e) {
-            System.out.println("Valor inválido!");
-        } finally {
-            input.nextLine(); // Limpar o buffer
-        }
+        } while(!sair);
     }
 
-    public static void checarAdmin(Scanner input, String email, String senha, boolean ehFuncAdmin) {
+    public static boolean checarAdmin(Scanner input, String email, String senha, boolean ehFuncAdmin) {
+        boolean login = false;
         String sql = "SELECT * FROM funcionario WHERE email = ? AND senha = ? AND adminf = ?";
 
         try (Connection connection = ConnectionFactory.getConnection();
@@ -107,14 +124,16 @@ public class Main {
             ResultSet result = statement.executeQuery();
 
             if (result.next()) {
-                mostrarMenu(input);
+                login = true;
             } else {
+                login = false;
                 System.out.println("Email ou senha incorretos!");
             }
-
+            
         } catch (SQLException e) {
             System.out.println("Erro ao conectar ao banco de dados ou executar a consulta!");
             e.printStackTrace();
         }
+        return login;
     }
 }
