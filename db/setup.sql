3ad2ad4cdb643a6e1ed3585d17a1d6f161edf1ea
johanpq <johanp.queiroz@gmail.com>
2024-10-08 15:19:28 -0300

    feat: adicionado a tabela hospede

--- db/setup.sql
+++ db/setup.sql
@@ -17,3 +17,14 @@ CREATE TABLE quarto (
 	status BOOLEAN not null,
 	PRIMARY KEY (numero)
 );
+
+-- Tabela Hospede
+CREATE TABLE hospede (
+	nome VARCHAR(30) not null,
+	documento VARCHAR(14) not null,
+	telefone VARCHAR(15) not null,
+	email VARCHAR(50) not null,
+	senha VARCHAR(10) not null,
+	alimento_restrito VARCHAR(20) not null,
+	PRIMARY KEY (documento)
+)
