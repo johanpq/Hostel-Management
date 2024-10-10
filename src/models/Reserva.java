52cd7888ec81ca7a1b35c8c59104d16b361e0c05
luizrodrigox <melo9657@gmail.com>
2024-10-10 14:46:18 -0300

    Atualizando reserva

--- src/models/Reserva.java
+++ src/models/Reserva.java
@@ -4,12 +4,17 @@ public class Reserva {
     private int numeroReserva;
     private String dataEntrada;
     private String dataSaida;
+    private Funcionario funcionario;
+    private Quarto quarto;
+    private Hospede hospede;
 
-
-    public Reserva(int numeroReserva, String dataEntrada, String dataSaida) {
+    public Reserva(int numeroReserva, String dataEntrada, String dataSaida, Funcionario funcionario, Quarto quarto, Hospede hospede) {
         this.numeroReserva = numeroReserva;
         this.dataEntrada = dataEntrada;
         this.dataSaida = dataSaida;
+        this.funcionario = funcionario;
+        this.quarto = quarto;
+        this.hospede = hospede;
     }
 
     public void setNumeroReserva(int numeroReserva) {
@@ -34,5 +39,43 @@ public class Reserva {
 
     public String getDataSaida() {
         return dataSaida;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
+
+    public Hospede getHospede() {
+        return hospede;
+    }
+
+    public void setHospede(Hospede hospede) {
+        this.hospede = hospede;
+    }
+
+    public String toString() {
+        String res = "";
+        res += "         Reserva         \n";
+        res += "=========================\n";
+        res += "Número da Reserva: " + getNumeroReserva() + "\n";
+        res += "Data de Entrada: " + getDataEntrada() + "\n";
+        res += "Data de Saída: " + getDataSaida() + "\n";
+        res += "Funcionario: " + funcionario.getNome() + "\n";
+        res += "Quarto: " + quarto.getNumero() + "\n";
+        res += "Hóspede: " + hospede.getNome() + "\n";
+        res += "=========================";
+        return res;
     }
 }
