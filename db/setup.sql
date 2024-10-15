73c75cb9f528506df7c6df574b7969d14553a6aa
srvl <servolo.silva@alunos.ufersa.edu.br>
2024-10-15 13:50:01 -0300

    criação da tabela reserva e modificação das demais

--- db/setup.sql
+++ db/setup.sql
@@ -1,8 +1,8 @@
 -- Tabela Funcionario
 CREATE TABLE funcionario (
 	nome VARCHAR(30) not null,
-	documento VARCHAR(14) not null,
-	telefone VARCHAR(15) not null,
+	documento CHAR(14) not null,
+	telefone CHAR(15) not null,
 	email VARCHAR(50) not null,
 	senha VARCHAR(10) not null,
 	cargo VARCHAR(30) not null,
@@ -21,10 +21,24 @@ CREATE TABLE quarto (
 -- Tabela Hospede
 CREATE TABLE hospede (
 	nome VARCHAR(30) not null,
-	documento VARCHAR(14) not null,
-	telefone VARCHAR(15) not null,
+	documento CHAR(14) not null,
+	telefone CHAR(15) not null,
 	email VARCHAR(50) not null,
 	senha VARCHAR(10) not null,
 	alimentoRestrito VARCHAR(20) not null,
 	PRIMARY KEY (documento)
-)
+);
+
+-- Tabela Reserva
+CREATE TABLE reserva (
+    numeroReserva INT NOT NULL,
+    dataEntrada DATE NOT NULL,
+    dataSaida DATE NOT NULL,
+    documentoFuncionario CHAR(14) NOT NULL,
+    numeroQuarto INT NOT NULL,
+    documentoHospede CHAR(14) NOT NULL,
+    PRIMARY KEY (numeroReserva),
+    FOREIGN KEY (documentoFuncionario) REFERENCES funcionario,
+    FOREIGN KEY (numeroQuarto) REFERENCES quarto,
+    FOREIGN KEY (documentoHospede) REFERENCES hospede
+);
