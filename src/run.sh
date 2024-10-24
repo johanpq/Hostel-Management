f7baedf9116b1db09e3a0da538aff179c871dcd1
johanpq <johanp.queiroz@gmail.com>
2024-10-23 22:13:16 -0300

    atualizado

--- src/run.sh
+++ src/run.sh
@@ -4,7 +4,7 @@
 mkdir -p ../bin
 
 # Compilar os arquivos Java
-javac -d ../bin -cp "lib/*" ../src/config/ConnectionFactory.java  ../src/models/Usuario.java ../src/models/Funcionario.java ../src/dao/FuncionarioDAO.java ../src/views/FuncionarioView.java ../src/models/Quarto.java ../src/dao/QuartoDAO.java ../src/views/QuartoView.java ../src/models/Hospede.java ../src/dao/HospedeDAO.java ../src/views/HospedeView.java ../src/Main.java
+javac -d ../bin -cp "lib/*" ../src/config/ConnectionFactory.java  ../src/models/Usuario.java ../src/models/Funcionario.java ../src/dao/FuncionarioDAO.java ../src/views/FuncionarioView.java ../src/models/Quarto.java ../src/dao/QuartoDAO.java ../src/views/QuartoView.java ../src/models/Hospede.java ../src/dao/HospedeDAO.java ../src/views/HospedeView.java ../src/models/Reserva.java ../src/dao/ReservaDAO.java ../src/views/ReservaView.java ../src/Main.java
 
 # Executar o programa principal
 java -cp "lib/*:../bin" Main
